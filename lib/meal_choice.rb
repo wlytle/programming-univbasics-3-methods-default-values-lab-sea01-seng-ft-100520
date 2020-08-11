395c24bb47d6915fd93ea8cb6a5675c605367e1f
Will Lytle <wlytle89@gmail.com>
2020-08-11 00:29:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,7 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-def meal_choice(veg1, veg2, protein="meat")
+def meal_choice(veg1, veg2, protein="tofu")
   puts "What a nutritious meal!" 
   order = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts order
